93eae19b221fb9c1daed7e61d681d0e98ab0b91d
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2017-11-19 18:15:48 +0000

    added altitude and heading, made timestamp human readable

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //when the jQuery Mobile page is initialised
 $(document).on('pageinit', function() {
-	
+
 	//set up listener for button click
 	$(document).on('click', getPosition);
-	
+
 	//change time box to show message
 	$('#time').val("Press the button to get location data");
-	
+
 });
 
 
 //Call this function when you want to get the current position
 function getPosition() {
-	
+
 	//change time box to show updated message
 	$('#time').val("Getting data...");
-	
+
 	//instruct location service to get position with appropriate callbacks
 	navigator.geolocation.getCurrentPosition(successPosition, failPosition);
 }
@@ -23,24 +23,31 @@ function getPosition() {
 
 //called when the position is successfully determined
 function successPosition(position) {
-	
+
 	//You can find out more details about what the position obejct contains here:
 	// http://www.w3schools.com/html/html5_geolocation.asp
-	
+
 
 	//lets get some stuff out of the position object
-	var time = position.timestamp;
+	var timestamp = position.timestamp;
 	var latitude = position.coords.latitude;
-	
+	var longitude=position.coords.longitude;
+	var altitude=position.coords.altitude;
+	var heading=position.coords.heading;
+	var time=new Date(timestamp);
+
 	//OK. Now we want to update the display with the correct values
 	$('#time').val("Recieved data at " + time);
-	$('#lattext').val("I should contain the latitude data...");
-	
+	$('#lattext').val(latitude);
+	$("#longtext").val(longitude);
+	$("#alttext").val(altitude);
+	$("#headtext").val(heading);
+
 }
 
 //called if the position is not obtained correctly
 function failPosition(error) {
 	//change time box to show updated message
 	$('#time').val("Error getting data: " + error);
-	
+
 }
